5b70f7ae628cf08a3ba7cb3e91c424554e2934eb
ks603 <kevin.shaughnessy18@gmail.com>
2021-01-26 21:32:19 -0500

    setup error message

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { lat: null}
+    this.state = { lat: null, errorMessage: '' }
 
     window.navigator.geolocation.getCurrentPosition(
-      position => {
+      (position) => {
         this.setState({ lat: position.coords.latitude })
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message })
+      }
     )
   }
 
-
-
-
-  render() { 
-    
-    return <div>Latitude: {this.state.lat}</div>
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    )
   }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
