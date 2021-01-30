6d1e023788aeed531569b51b687aa6e6fb67a845
ks603 <kevin.shaughnessy18@gmail.com>
2021-01-30 12:42:32 -0500

    Added a Spinner component for page loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spnner'
 import 'semantic-ui-css/semantic.min.css'
 
 class App extends React.Component {
@@ -24,7 +25,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <div>Loading!</div>
+    return <Spinner />
   }
 }
 
