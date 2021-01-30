021da4ddf5576b55db51d41ebaaf9fcf913d86a6
ks603 <kevin.shaughnessy18@gmail.com>
2021-01-30 10:49:56 -0500

    Tells you what season it is based on the month

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+let timeOfyear = new Date().getMonth()
+
 const SeasonDisplay = (props) => {
   console.log(props.lat)
-  return ( <div>Season Display</div>
+  
+  if (timeOfyear >= 3 &&  timeOfyear <= 8) {
+  return ( <div>its summer</div>
   )
+  } else {
+    return (<div>its winter</div>)
+  }
 }
 
 
